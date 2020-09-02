fix numerical stability of WSDDN loss; improve demo

maskrcnn_benchmark/modeling/roi_heads/wsddn_head/loss.py
@@ -24,7 +24,7 @@ class WSDDNLossComputation(object):
         box_class_logits = class_logits.split(num_box_per_img, dim=0)
         image_class_logits = [torch.logsumexp(l, dim=0) for l in box_class_logits]
         image_class_logits = torch.stack(image_class_logits, dim=0)
-        negative_logits = torch.log(1.0 - torch.exp(image_class_logits))
+        negative_logits = torch.log(1.0 - torch.exp(image_class_logits) + 1e-6)
         classification_loss = (- (targets * image_class_logits) - 
             ((1 - targets) * negative_logits * self.background_weight))
         classification_loss = classification_loss.mean()

maskrcnn_benchmark/utils/logged_module.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.distributed as dist
 
 def stats(tensor):
     t = tensor.cpu().detach().numpy()
@@ -23,14 +24,14 @@ class LoggedModule(nn.Module):
         s = stats(tensor)
         self.log_info[name] = s
         if self._log_print:
-            print(name, stats(tensor))
+            print(f'RANK {dist.get_rank()}: {name}', s)
         if self._log_raise_nan and torch.isnan(tensor).any():
             raise ValueError()
 
     def log_dict(self, d):
         self.log_info.update(d)
         if self._log_print:
-            print(d)
+            print(f'RANK {dist.get_rank()}: {d}')
         if self._log_raise_nan:
             for v in d.values():
                 if torch.isnan(v).any():